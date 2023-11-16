b60169c1e0ee534d79e7eb77511e7b6cb5043693
Rifat <rifatkhabibulin88@gmail.comgit config --global user.name Rifatgit statusgit config user.email rifatkhabibulin88@gmail.com>
2023-11-16 13:27:25 +0400

    feat: create layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container>
+    <el-header>
+      <app-navigation />
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer />
+</el-container>
+</template>
+
+<script>
+import AppNavigation from '@/components/main/Navigation.vue';
+
+export default {
+  components: {AppNavigation}
+}
+</script>
